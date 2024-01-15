b72459ea7b424b6b712ac0ad11be1d860cf88335
roc102 <kyroc102@gmail.com>
2024-01-15 17:22:02 +0530

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+import { thunk } from 'redux-thunk';
+
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
@@ -14,7 +15,7 @@ const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobots,requestRobots })
 const store = 
-  createStore(rootReducer,applyMiddleware(thunkMiddleware,logger))
+  createStore(rootReducer,applyMiddleware(thunk,logger))
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
